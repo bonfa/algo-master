46933d71a1b573ccec86116f21632f27bb2d7d26
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-03-19 14:59:07 +0100

    add HashTablesMenuPrinter

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/ui/category/HashTablesMenuPrinter.kt
@@ -0,0 +1,25 @@
+package it.fbonfadelli.algo.master.ui.category
+
+import it.fbonfadelli.algo.master.exercises.hash.tables.MaxNumberOfBalloons
+import it.fbonfadelli.algo.master.ui.Common
+
+object HashTablesMenuPrinter {
+
+    fun execute() {
+        println("HASH TABLES:")
+        println("[2] - Max Number Of Balloons")
+        println()
+        println(Common.Messages.EXIT)
+        println(Common.Messages.BACK)
+        println(Common.Messages.ASK_FOR_COMMAND)
+
+        val input = readlnOrNull()?.trim()?.uppercase()
+
+        when (input) {
+            "2" -> MaxNumberOfBalloons.main()
+            Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
+            Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
+            else -> Common.Actions.exitWithError()
+        }
+    }
+}
