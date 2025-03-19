8ec51411b1413158ae7210803cda180aeff176ea
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-03-19 16:15:01 +0100

    add IsomorphicStrings

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/ui/category/HashTablesMenuPrinter.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/ui/category/HashTablesMenuPrinter.kt
@@ -1,5 +1,6 @@
 package it.fbonfadelli.algo.master.ui.category
 
+import it.fbonfadelli.algo.master.exercises.hash.tables.IsomorphicStrings
 import it.fbonfadelli.algo.master.exercises.hash.tables.MaxNumberOfBalloons
 import it.fbonfadelli.algo.master.exercises.hash.tables.NumberOfGoodPairs
 import it.fbonfadelli.algo.master.ui.Common
@@ -10,6 +11,7 @@ object HashTablesMenuPrinter {
         println("HASH TABLES:")
         println("[2] - Max Number Of Balloons")
         println("[3] - Number of good pairs")
+        println("[4] - Isomorphic Strings")
         println()
         println(Common.Messages.EXIT)
         println(Common.Messages.BACK)
@@ -20,6 +22,7 @@ object HashTablesMenuPrinter {
         when (input) {
             "2" -> MaxNumberOfBalloons.main()
             "3" -> NumberOfGoodPairs.main()
+            "4" -> IsomorphicStrings.main()
             Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
             Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
             else -> Common.Actions.exitWithError()
