56fe66b80e4c4245cf938bdf2148128bec35748d
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-03-19 15:19:14 +0100

    add NumberOfGoodPairs

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/ui/category/HashTablesMenuPrinter.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/ui/category/HashTablesMenuPrinter.kt
@@ -1,6 +1,7 @@
 package it.fbonfadelli.algo.master.ui.category
 
 import it.fbonfadelli.algo.master.exercises.hash.tables.MaxNumberOfBalloons
+import it.fbonfadelli.algo.master.exercises.hash.tables.NumberOfGoodPairs
 import it.fbonfadelli.algo.master.ui.Common
 
 object HashTablesMenuPrinter {
@@ -8,6 +9,7 @@ object HashTablesMenuPrinter {
     fun execute() {
         println("HASH TABLES:")
         println("[2] - Max Number Of Balloons")
+        println("[3] - Number of good pairs")
         println()
         println(Common.Messages.EXIT)
         println(Common.Messages.BACK)
@@ -17,6 +19,7 @@ object HashTablesMenuPrinter {
 
         when (input) {
             "2" -> MaxNumberOfBalloons.main()
+            "3" -> NumberOfGoodPairs.main()
             Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
             Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
             else -> Common.Actions.exitWithError()
