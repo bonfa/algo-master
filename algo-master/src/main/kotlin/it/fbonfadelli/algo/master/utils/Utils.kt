b394e19c8d01efca89a0e865512ea6ca42c05eac
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-03-20 08:56:00 +0100

    add GroupAnagrams

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/utils/Utils.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/utils/Utils.kt
@@ -3,5 +3,8 @@ package it.fbonfadelli.algo.master.utils
 fun IntArray.printable() =
     "[" + this.joinToString((",")) + "]"
 
+fun Array<String>.printable() =
+    "[" + this.joinToString((",")) + "]"
+
 fun <T> List<T>.printable() =
     "[" + this.joinToString((",")) + "]"
