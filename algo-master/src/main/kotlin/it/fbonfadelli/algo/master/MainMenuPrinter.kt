5fb1a943127832065ee94ec16510aa13f1135d2b
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-03-23 10:21:01 +0100

    add TwoPointersMenuPrinter to MainMenuPrinter

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/MainMenuPrinter.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/MainMenuPrinter.kt
@@ -4,6 +4,7 @@ import it.fbonfadelli.algo.master.arrays.ui.ArrayMenuPrinter
 import it.fbonfadelli.algo.master.bit.manipulation.ui.BitManipulationMenuPrinter
 import it.fbonfadelli.algo.master.hash.tables.ui.HashTablesMenuPrinter
 import it.fbonfadelli.algo.master.strings.ui.StringMenuPrinter
+import it.fbonfadelli.algo.master.two.pointers.ui.TwoPointersMenuPrinter
 
 object MainMenuPrinter {
 
@@ -18,6 +19,7 @@ object MainMenuPrinter {
         println("[2] - Strings")
         println("[3] - Bit Manipulation")
         println("[4] - Hash Tables")
+        println("[5] - Two Pointers")
         println()
         println(Common.Messages.EXIT)
 
@@ -28,6 +30,7 @@ object MainMenuPrinter {
             "2" -> StringMenuPrinter.execute()
             "3" -> BitManipulationMenuPrinter.execute()
             "4" -> HashTablesMenuPrinter.execute()
+            "5" -> TwoPointersMenuPrinter.execute()
             Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
             else -> Common.Actions.exitWithError()
         }
