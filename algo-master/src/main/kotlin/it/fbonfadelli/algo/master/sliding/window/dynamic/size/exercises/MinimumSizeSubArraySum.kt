9cb4216275c5c9a7e63b0b00dd853208b32b8d93
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-04-25 13:05:32 +0200

    implement MinimumSizeSubArraySum

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/sliding/window/dynamic/size/exercises/MinimumSizeSubArraySum.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/sliding/window/dynamic/size/exercises/MinimumSizeSubArraySum.kt
@@ -11,7 +11,7 @@ class MinimumSizeSubArraySum {
             val inputs = listOf(
                 Input(7, intArrayOf(2,3,1,2,4,3)),
                 Input(4, intArrayOf(1,4,4)),
-                Input(1, intArrayOf(1,1,1,1,1,1,1,1)),
+                Input(11, intArrayOf(1,1,1,1,1,1,1,1)),
             )
 
             inputs.forEachIndexed { index, input -> execute(index, input) }
@@ -27,7 +27,40 @@ class MinimumSizeSubArraySum {
     }
 
     fun minSubArrayLen(target: Int, nums: IntArray): Int {
-        TODO()
+        if (nums.size == 1 && nums[0] >= target) {
+            return 1
+        } else if (nums.size == 1 && nums[0] < target) {
+            return 0
+        } else {
+            var minLength = Int.MAX_VALUE
+            var l = 0
+            var r = l+1
+            var currentSum = nums[l]
+
+            if (currentSum >= target)
+                return 1
+
+            while(r < nums.size) {
+                currentSum += nums[r]
+                if (currentSum >= target) {
+                    minLength = Math.min(minLength, r - l + 1)
+                }
+
+                while ((currentSum - nums[l]) >= target && l <= r) {
+                    currentSum -= nums[l]
+                    l++
+                    minLength = Math.min(minLength, r - l + 1)
+                }
+
+                if (minLength == 1) {
+                    break
+                }
+
+                r++
+            }
+
+            return if (minLength == Int.MAX_VALUE) 0 else minLength
+        }
     }
 
     private data class Input(val target: Int, val nums: IntArray)
