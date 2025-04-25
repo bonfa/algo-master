6e6615fa2d48a24e2209aa1a57aaa07db0746d1b
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-04-25 10:46:11 +0200

    add SlidingWindowDynamicSizeMenuPrinter

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/sliding/window/dynamic/size/ui/SlidingWindowDynamicSizeMenuPrinter.kt
@@ -0,0 +1,33 @@
+package it.fbonfadelli.algo.master.sliding.window.dynamic.size.ui
+
+import it.fbonfadelli.algo.master.Common
+import it.fbonfadelli.algo.master.sliding.window.fixed.size.exercises.FindAllAnagramsInAString
+import it.fbonfadelli.algo.master.sliding.window.fixed.size.exercises.MaximumAverageSubArrayI
+import it.fbonfadelli.algo.master.sliding.window.fixed.size.exercises.MaximumSumOfDistinctSubArraysOfLengthK
+import it.fbonfadelli.algo.master.sliding.window.fixed.size.exercises.PermutationInString
+
+object SlidingWindowDynamicSizeMenuPrinter {
+
+    fun execute() {
+        println("SLIDING WINDOW DYNAMIC SIZE OPTIONS:")
+        println("[1] - Longest Substring Without Repeating Characters")
+        println()
+        println(Common.Messages.EXIT)
+        println(Common.Messages.BACK)
+        println(Common.Messages.ASK_FOR_COMMAND)
+
+        val input = readlnOrNull()?.trim()?.uppercase()
+
+        when (input) {
+//            "1" -> MaximumAverageSubArrayI.main()
+            Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
+            Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
+            else -> Common.Actions.exitWithError()
+        }
+    }
+}
+
+
+
+
+
