1d10c06279e9219246ef6cdd98a504cf9d9000d5
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-05-02 16:04:14 +0200

    clean up NumberOfZeroFilledSubArrays

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/arrays/exercises/NumberOfZeroFilledSubArrays.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/arrays/exercises/NumberOfZeroFilledSubArrays.kt
@@ -8,7 +8,7 @@ class NumberOfZeroFilledSubArrays {
         private val numberOfZeroFilledSubArrays = NumberOfZeroFilledSubArrays()
 
         fun main() {
-            println("NUMBER_OF_ZERO_FILLED_SUBARRAYS")
+            println("NUMBER OF ZERO FILLED SUB ARRAYS")
 
             val inputs = listOf(
                 intArrayOf(1,3,0,0,2,0,0,4),
@@ -25,17 +25,13 @@ class NumberOfZeroFilledSubArrays {
         private fun execute(index: Int, input: IntArray) {
             println("CASE ${index+1}:")
             println("Input: " + input.printable())
-            val me = numberOfZeroFilledSubArrays.numberOfZeroFilledSubArrays(input)
+            val me = numberOfZeroFilledSubArrays.execute(input)
             println("Output: $me")
             println()
         }
     }
 
-    fun numberOfZeroFilledSubArrays(nums: IntArray): Long {
-        return version2(nums)
-    }
-
-    private fun version2(nums: IntArray): Long {
+    fun execute(nums: IntArray): Long {
         var count = 0L
         var numOfPreviousZeros = 0L
         for (num in nums) {
@@ -51,36 +47,4 @@ class NumberOfZeroFilledSubArrays {
 
         return count
     }
-
-    private fun version1(nums: IntArray): Long {
-        var occurrences = 0L
-        var lengthOfZeroSequence = 0L
-
-        for (i in 0..nums.size - 1) {
-            val num = nums[i]
-            if (num == 0) {
-                lengthOfZeroSequence++
-            }
-
-            if (num != 0 || i == nums.size - 1) {
-                val occurrensesForCurrentSequence: Long = lengthOfZeroSequence * (lengthOfZeroSequence + 1) / 2
-                occurrences = occurrences + occurrensesForCurrentSequence
-                lengthOfZeroSequence = 0
-            }
-        }
-        return occurrences
-    }
-
-    // [0,0,0] --> 3 * [0] + 2 * [0,0] + 1 * [0,0,0] --> 3 * (3+1) / 2 = 6
-    // for each sequence of 0s,
-    //   - I calculate the length
-    //   - apply the formula
-    // checking the edge cases
-    // - [0] --> 1 --> 1*2 / 2 = 1
-    // - [] --> 0
-
-    // checking edge cases (100_000 items)
-    //   705_082_704
-    // 5_000_050_000
-    // Integer.MAX_VALUE 2_147_483_647
 }
