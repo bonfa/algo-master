89ae680a135101b3627568d057fd4a445ec181aa
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-05-29 20:46:23 +0200

    new exercise PartitionList

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/linked/list/exercises/PartitionList.kt
@@ -0,0 +1,74 @@
+package it.fbonfadelli.algo.master.linked.list.exercises
+
+import it.fbonfadelli.algo.master.linked.list.util.ListNode
+
+class PartitionList {
+
+    companion object {
+        private val partitionList = PartitionList()
+
+        fun main() {
+            println("PARTITION LIST")
+
+            val inputs = listOf(
+                Input(intArrayOf(1,4,3,2,5,2), 3),
+                Input(intArrayOf(2,1), 2),
+            )
+
+            inputs.forEachIndexed { index, input -> execute(index, input) }
+        }
+
+        private fun execute(index: Int, input: Input) {
+            println("CASE ${index+1}:")
+            println("Input: $input")
+            val output = partitionList.execute(input.linkedList(), input.x)
+            println("Output: ${output?.printable()}")
+            println()
+        }
+    }
+
+    fun execute(head: ListNode?, x: Int): ListNode? {
+        var smallerHead: ListNode? = ListNode(-1000)
+        var otherHead: ListNode? = ListNode(-1000)
+
+        var node = head
+        var smallerNode = smallerHead!!
+        var otherNode = otherHead!!
+        while(node != null) {
+            if (node.`val` < x) {
+                smallerNode.next = node
+                smallerNode = smallerNode.next!!
+            } else { // >= x
+                otherNode.next = node
+                otherNode = otherNode.next!!
+            }
+
+            node = node.next
+        }
+
+        smallerHead = smallerHead.next
+        otherHead = otherHead.next
+
+        if (smallerHead == null)
+            return otherHead
+        else if (otherHead == null)
+            return smallerHead
+        else {
+            smallerNode.next = otherHead
+            otherNode.next = null
+            return smallerHead
+        }
+    }
+
+    private data class Input(private val array: IntArray, val x: Int) {
+        fun linkedList(): ListNode {
+            val head: ListNode? = ListNode(array.first())
+            var node = head
+            for (i in 1 until array.size) {
+                node!!.next = ListNode(array[i])
+                node = node.next
+            }
+            return head!!
+        }
+    }
+}
--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/linked/list/ui/LinkedListMenuPrinter.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/linked/list/ui/LinkedListMenuPrinter.kt
@@ -4,6 +4,7 @@ import it.fbonfadelli.algo.master.Common
 import it.fbonfadelli.algo.master.linked.list.exercises.CopyListWithRandomPointer
 import it.fbonfadelli.algo.master.linked.list.exercises.DesignLinkedList
 import it.fbonfadelli.algo.master.linked.list.exercises.IntersectionOfTwoLinkedLists
+import it.fbonfadelli.algo.master.linked.list.exercises.PartitionList
 import it.fbonfadelli.algo.master.linked.list.exercises.RemoveDuplicatesFromSortedListII
 import it.fbonfadelli.algo.master.linked.list.exercises.RemoveNthFromEndOfList
 import it.fbonfadelli.algo.master.linked.list.exercises.SwapNodesInPairs
@@ -18,6 +19,7 @@ object LinkedListMenuPrinter {
         println("[4] - Remove Duplicates From Sorted List II")
         println("[5] - Swap nodes in pairs")
         println("[6] - Copy list with random pointer")
+        println("[7] - Partition list")
         println()
         println(Common.Messages.EXIT)
         println(Common.Messages.BACK)
@@ -32,6 +34,7 @@ object LinkedListMenuPrinter {
             "4" -> RemoveDuplicatesFromSortedListII.main()
             "5" -> SwapNodesInPairs.main()
             "6" -> CopyListWithRandomPointer.main()
+            "7" -> PartitionList.main()
             Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
             Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
             else -> Common.Actions.exitWithError()
