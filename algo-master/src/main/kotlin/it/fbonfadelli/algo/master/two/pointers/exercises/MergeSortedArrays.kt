4f5301253340ef15bff7aad8cc63bef62542aae2
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-07-12 11:11:43 +0200

    new version of MergeSortedArrays

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/two/pointers/exercises/MergeSortedArrays.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/two/pointers/exercises/MergeSortedArrays.kt
@@ -33,81 +33,25 @@ class MergeSortedArrays {
     }
 
     private fun version2(m: Int, n: Int, nums1: IntArray, nums2: IntArray) {
-        if (n == 0)
-            return
-
+        val temp = IntArray(m + n)
         var p1 = 0
         var p2 = 0
 
-        while (p1 < m+n) {
-            if (p1 < m && p2 < n) {
-                if (nums1[p1] <= nums2[p2]) {
-                    p1++
-                } else {
-                    var temp = nums1[p1]
-                    nums1[p1] = nums2[p2]
-                    nums2[p2] = temp
-
-                    var p2Temp = p2
-                    while(p2Temp < n-1 && nums2[p2Temp+1] < nums2[p2Temp]) {
-                        var t = nums2[p2Temp]
-                        nums2[p2Temp] = nums2[p2Temp+1]
-                        nums2[p2Temp+1] = t
-                        p2Temp++
-                    }
-                }
-            } else {
-                nums1[p1] = nums2[p2]
-                p1++
+        for(i in 0 until m + n) {
+            if (p1 >= m || (p2 < n && nums1[p1] > nums2[p2])) {
+                temp[i] = nums2[p2]
                 p2++
-            }
-        }
-    }
-
-
-    private fun version1(m: Int, n: Int, nums1: IntArray, nums2: IntArray) {
-        //create result
-        val result = IntArray(m + n)
-        var i = 0
-        var j = 0
-        var k = 0
-        while (true) {
-            var num1: Int? = null
-            if (i < m) {
-                num1 = nums1[i]
-            }
-
-            var num2: Int? = null
-            if (j < n) {
-                num2 = nums2[j]
-            }
-
-            if (num1 == null) {
-                result[k] = num2!!
-                j++
-            } else if (num2 == null) {
-                result[k] = num1!!
-                i++
-            } else if (num1!! <= num2!!) {
-                result[k] = num1
-                i++
             } else {
-                result[k] = num2
-                j++
+                temp[i] = nums1[p1]
+                p1++
             }
-
-            k++
-
-            if (i >= m && j >= n)
-                break
         }
 
-
-        //copy result in nums1
-        for (i in 0..result.size - 1) {
-            nums1[i] = result[i]
+        for(i in 0 until m + n) {
+            nums1[i] = temp[i]
         }
     }
+
 
     private data class Input(val nums1: IntArray, val m:Int, val nums2: IntArray, val n: Int)
 }
