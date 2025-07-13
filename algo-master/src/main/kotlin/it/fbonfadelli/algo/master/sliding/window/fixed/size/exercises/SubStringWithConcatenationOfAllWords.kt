69e1a06a0da18d5ba1944a1863789b3c11f9deae
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-07-13 12:09:19 +0200

    new version of SubStringWithConcatenationOfAllWords

--- algo-master/src/main/kotlin/it/fbonfadelli/algo/master/sliding/window/fixed/size/exercises/SubStringWithConcatenationOfAllWords.kt
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/sliding/window/fixed/size/exercises/SubStringWithConcatenationOfAllWords.kt
@@ -13,6 +13,7 @@ class SubStringWithConcatenationOfAllWords {
                 Input("wordgoodgoodgoodbestword", arrayOf("word","good","best","word")),
                 Input("barfoofoobarthefoobarman", arrayOf("bar","foo","the")),
                 Input("wordgoodgoodgoodbestword", arrayOf("word","good","best","good")),
+                Input("lingmindraboofooowingdingbarrwingmonkeypoundcake", arrayOf("fooo","barr","wing","ding","wing")),
             )
 
             inputs.forEachIndexed { index, input -> execute(index, input) }
@@ -28,50 +29,56 @@ class SubStringWithConcatenationOfAllWords {
     }
 
     fun execute(s: String, words: Array<String>): List<Int> {
-        val wordsFreq = mutableMapOf<String, Int>()
-        for (word in words) {
-            wordsFreq[word] = wordsFreq.getOrDefault(word, 0) + 1
-        }
+        val wordsMap = createWordsMap(words)
+        val wl = words[0].length
+        val ws = words.size
+        val k = wl * ws
 
-        val lengthOfSingleWord = words[0].length
-        val numberOfWords = words.size
+        val indexes = mutableListOf<Int>()
 
         var i = 0
-        val matchingIndexes = mutableListOf<Int>()
-        while (i < s.length) {
-
-            if (matches(s, i, lengthOfSingleWord, wordsFreq, words.size))
-                matchingIndexes.add(i)
-
+        while(i + k <= s.length) {
+            if (startsConcatenatedString(i, wl, ws, wordsMap, s)) {
+                indexes.add(i)
+            }
             i++
         }
 
-        return matchingIndexes
+        return indexes
     }
 
-    private fun matches(s: String, i: Int, lengthOfSingleWord: Int, wordsFreq: Map<String, Int>, numberOfWords: Int): Boolean {
-        val candidateFreq = mutableMapOf<String, Int>()
+    private fun createWordsMap(words: Array<String>): Map<String, Int> {
+        val map = mutableMapOf<String, Int>()
 
-        var n = 0
-        var j = i
-        while(n < numberOfWords && j + lengthOfSingleWord <= s.length) {
-            val candidateWord = s.substring(j, j + lengthOfSingleWord)
-            if (!wordsFreq.contains(candidateWord))
-                return false
-
-            candidateFreq[candidateWord] = candidateFreq.getOrDefault(candidateWord, 0) + 1
-            j += lengthOfSingleWord
-            n++
+        for (word in words) {
+            map[word] = map.getOrDefault(word, 0) + 1
         }
 
-
-        return mapEquals(candidateFreq, wordsFreq)
+        return map
     }
 
-    private fun mapEquals(m1: Map<String, Int>, m2: Map<String, Int>): Boolean {
-        if (m1.keys != m2.keys)
-            return false
-        return m1.keys.all { m1[it] == m2[it] }
+    private fun startsConcatenatedString(
+        startIdx: Int,
+        wl: Int,
+        ws: Int,
+        wordsMap: Map<String, Int>,
+        s: String
+    ): Boolean {
+        var i = startIdx
+        val sMap = mutableMapOf<String, Int>()
+        while (i < startIdx + wl * ws) {
+            val sWord = s.substring(i, i + wl)
+            if (!wordsMap.containsKey(sWord)) {
+                return false
+            } else { //wordsMap contains sword
+                sMap[sWord] = sMap.getOrDefault(sWord, 0) + 1
+                if (sMap[sWord]!! > wordsMap[sWord]!!) {
+                    return false
+                }
+            }
+            i += wl
+        }
+        return true
     }
 
     private data class Input(val s: String, val words: Array<String>)
