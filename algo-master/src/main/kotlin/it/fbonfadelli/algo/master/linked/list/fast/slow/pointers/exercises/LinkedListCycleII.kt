3e195338c99f3bab5360a15b3d22a4be9a0bc6cd
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-08-02 11:05:50 +0200

    new exercise LinkedListCycleII

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/linked/list/fast/slow/pointers/exercises/LinkedListCycleII.kt
@@ -0,0 +1,81 @@
+package it.fbonfadelli.algo.master.linked.list.fast.slow.pointers.exercises
+
+import it.fbonfadelli.algo.master.linked.list.util.ListNode
+
+class LinkedListCycleII {
+
+    companion object {
+        private val linkedListCycleII = LinkedListCycleII()
+
+        fun main() {
+            println("LINKED LIST - CYCLE 2")
+
+            val inputs = listOf(
+                Input(intArrayOf(3,2,0,-4), 1),
+                Input(intArrayOf(1,2), 0),
+                Input(intArrayOf(1), -1),
+            )
+
+            inputs.forEachIndexed { index, input -> execute(index, input) }
+        }
+
+        private fun execute(index: Int, input: Input) {
+            println("CASE ${index + 1}:")
+            println("Input: $input")
+            val output = linkedListCycleII.execute(input.linkedList())
+            println("Output: ${output?.printable()}")
+            println()
+        }
+    }
+
+    private fun execute(head: ListNode?): ListNode? {
+        var fast = head
+        var slow = head
+        val seen = mutableSetOf<ListNode>()
+
+        while(fast != null) {
+            seen.add(slow!!)
+
+            slow = slow.next
+
+            if (seen.contains(slow))
+                return slow
+
+            if (fast.next != null && seen.contains(fast.next))
+                return fast.next
+
+            if (fast.next?.next != null && seen.contains(fast.next!!.next))
+                return fast.next!!.next
+
+            fast = fast.next?.next
+        }
+
+        return null
+    }
+
+    private data class Input(private val array: IntArray, private val indexOfTheStartOfTheCycle: Int) {
+        fun linkedList(): ListNode? {
+            if (array.isEmpty()) return null
+
+            val head: ListNode? = ListNode(array.first())
+            var node = head
+            var startOfCycle: ListNode? = null
+            if (indexOfTheStartOfTheCycle == 0)
+                startOfCycle = head
+
+            for (i in 1 until array.size) {
+                val newNode = ListNode(array[i])
+                if (i == indexOfTheStartOfTheCycle)
+                    startOfCycle = newNode
+                node!!.next = newNode
+                node = node.next
+            }
+
+            if (startOfCycle != null) {
+                node?.next = startOfCycle
+            }
+
+            return head!!
+        }
+    }
+}
