0d46c95e32c646a8ded6cddbff8b328ee408841a
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-08-04 08:23:20 +0200

    new stack menu printer

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/stack/ui/StackMenuPrinter.kt
@@ -0,0 +1,38 @@
+package it.fbonfadelli.algo.master.stack.ui
+
+import it.fbonfadelli.algo.master.Common
+import it.fbonfadelli.algo.master.stack.exercises.ValidParentheses
+
+object StackMenuPrinter {
+
+    fun execute() {
+        println("SLIDING WINDOW FIXED SIZE OPTIONS:")
+        println("1. Valid Parentheses")
+//        println("2. Remove All Adjacent Duplicates In String")
+//        println("3. Min Stack")
+//        println("4. Remove Duplicate Letters")
+//        println("5. Removing Stars From a String")
+//        println("6. Evaluate Reverse Polish Notation")
+//        println("7. Basic Calculator II")
+//        println("8. Longest Valid Parentheses")
+        println()
+        println(Common.Messages.EXIT)
+        println(Common.Messages.BACK)
+        println(Common.Messages.ASK_FOR_COMMAND)
+
+        val input = readlnOrNull()?.trim()?.uppercase()
+
+        when (input) {
+            "1" -> ValidParentheses.main()
+
+            Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
+            Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
+            else -> Common.Actions.exitWithError()
+        }
+    }
+}
+
+
+
+
+
