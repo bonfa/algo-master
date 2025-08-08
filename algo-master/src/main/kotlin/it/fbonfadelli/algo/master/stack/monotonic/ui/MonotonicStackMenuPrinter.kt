606add1ae75c9d3d6a5ae74de2d92b21d815c987
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-08-08 16:37:06 +0200

    new MonotonicStackMenuPrinter menu printer

---
+++ algo-master/src/main/kotlin/it/fbonfadelli/algo/master/stack/monotonic/ui/MonotonicStackMenuPrinter.kt
@@ -0,0 +1,38 @@
+package it.fbonfadelli.algo.master.stack.monotonic.ui
+
+import it.fbonfadelli.algo.master.Common
+import it.fbonfadelli.algo.master.stack.generic.exercises.RemoveAllAdjacentDuplicatesInString
+import it.fbonfadelli.algo.master.stack.generic.exercises.RemovingStarsFromAString
+import it.fbonfadelli.algo.master.stack.generic.exercises.ValidParentheses
+
+object MonotonicStackMenuPrinter {
+
+    fun execute() {
+        println("MONOTONIC STACK OPTIONS:")
+        println("1. Next Greater Element I")
+        println("2. Daily Temperatures")
+        println("3. Online Stock Span")
+        println("4. 132 Pattern")
+        println("5. Number of Visible People in a Queue")
+        println("6. Largest Rectangle in Histogram")
+        println()
+        println(Common.Messages.EXIT)
+        println(Common.Messages.BACK)
+        println(Common.Messages.ASK_FOR_COMMAND)
+
+        val input = readlnOrNull()?.trim()?.uppercase()
+
+        when (input) {
+//            "1" -> ValidParentheses.main()
+
+            Common.Commands.EXIT -> Common.Actions.sayGoodByeAndExit()
+            Common.Commands.BACK -> Common.Actions.goBackToMainMenu()
+            else -> Common.Actions.exitWithError()
+        }
+    }
+}
+
+
+
+
+
